0cb589f52b193afb6e6e5129b05d0edd8a0333c9
Alaa Hamdy <99442728+alaa724@users.noreply.github.com>
2024-03-20 11:04:36 +0200

    DAL For Department Model

---
+++ Route.C41.G01.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C41.G01.DAL.Data.Configurations;
+using Route.C41.G01.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.G01.DAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = .; Database = MVCAplicationG01; Trusted_Connection = True;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
