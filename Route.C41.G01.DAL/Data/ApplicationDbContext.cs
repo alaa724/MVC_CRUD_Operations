59e37f9492892bc20dbc55122c230e4b9906372f
Alaa Hamdy <99442728+alaa724@users.noreply.github.com>
2024-03-23 12:40:50 +0200

    Part05 Employee Module - DAL

--- Route.C41.G01.DAL/Data/ApplicationDbContext.cs
+++ Route.C41.G01.DAL/Data/ApplicationDbContext.cs
@@ -24,9 +24,12 @@ namespace Route.C41.G01.DAL.Data
         {
 
             //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            //modelBuilder.ApplyConfiguration<Employee>(new EmployeeConfigurations());
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
 
         public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
     }
 }
---
+++ Route.C41.G01.DAL/Models/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.G01.DAL.Models
+{
+    public enum Gender
+    {
+        [EnumMember(Value ="Male")]
+        Male = 1,
+        [EnumMember(Value = "Female")]
+        Female = 2
+    }
+
+    public enum ContractType
+    {
+        FullTime = 1,
+        PartTime = 2
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name is required !!")]
+        [MaxLength(50 , ErrorMessage ="Max length of name is 50 char")]
+        [MinLength(4 , ErrorMessage = "Min length of name is 4 char")]
+        public string Name { get; set; }
+
+        [Range(22 , 60)]
+        public int? Age { get; set; }
+
+        public Gender Gender { get; set; }
+
+        [RegularExpression(@"^(\\d{1,}) [a-zA-Z0-9\\s]+(\\,)? [a-zA-Z]+(\\,)? [A-Z]{2} [0-9]{5,6}$"
+            , ErrorMessage ="Address Must be like 123-Street-City-Country")]
+        public string Address { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Salary { get; set; }
+
+        [Display(Name ="Is Active")]
+        public bool IsActive { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name ="Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name ="Hiring Date")]
+        public DateTime HiringDate { get; set; }
+
+        public ContractType ContractType { get; set; }
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
