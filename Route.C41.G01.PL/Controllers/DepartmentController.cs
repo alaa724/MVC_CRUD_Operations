19bb2a494c4c2727fa984cdba934302b7ce38892
Alaa Hamdy <99442728+alaa724@users.noreply.github.com>
2024-03-23 09:30:51 +0200

    Part02 Department Controller - Edit

--- Route.C41.G01.PL/Controllers/DepartmentController.cs
+++ Route.C41.G01.PL/Controllers/DepartmentController.cs
@@ -1,22 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Route.C41.G01.BL.Interfaces;
 using Route.C41.G01.BL.Repositories;
 using Route.C41.G01.DAL.Models;
+using System;
 
 namespace Route.C41.G01.PL.Controllers
 {
     public class DepartmentController : Controller
     {
         private readonly IDepartmentRepository _departmentRepo;
+        private readonly IWebHostEnvironment _env;
 
         // Inheritance : DepartmentRepository is a Controller
         // Composation : DepartmentController has a DepartmentRepository
 
 
-        public DepartmentController(IDepartmentRepository departmentRepo) // Ask CLR for creating an object from class implementing "IDepartmentRepository" Interface
+        public DepartmentController(IDepartmentRepository departmentRepo , IWebHostEnvironment env) // Ask CLR for creating an object from class implementing "IDepartmentRepository" Interface
         {
 
             _departmentRepo = departmentRepo;
+            _env = env;
         }
 
 
@@ -30,7 +35,7 @@ namespace Route.C41.G01.PL.Controllers
         }
 
         // /Depertment/Create
-        [HttpGet]
+        //[HttpGet]
         public IActionResult Create()
         {
             return View();
@@ -51,8 +56,8 @@ namespace Route.C41.G01.PL.Controllers
 
         // Department/Details/10
         // Department/Details
-        [HttpGet]
-        public IActionResult Details(int? id)
+        //[HttpGet]
+        public IActionResult Details(int? id, string ViewName = "Details")
         {
             if (!id.HasValue)
                 return BadRequest(); // 400
@@ -61,8 +66,53 @@ namespace Route.C41.G01.PL.Controllers
             if (department is null)
                 return NotFound();  // 404
 
-            return View(department);
+            return View(ViewName,department);
 
+        }
+
+        // Department/Edit/10
+        // Department/Edit
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            return Details(id , "Edit");
+
+            ///if (!id.HasValue)
+            ///    return BadRequest(); // 400
+            ///var department = _departmentRepo.Get(id.Value);
+            ///if (department is null)
+            ///    return NotFound(); // 404
+            ///return View(department);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute] int id,Department department)
+        {
+            if (id != department.Id)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return View(department);
+
+            try
+            {
+                _departmentRepo.Update(department);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                // 1. Log Exception
+                // 2. Frindly Message
+
+                if(_env.IsDevelopment())
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                else
+                    ModelState.AddModelError(string.Empty, "An Error Has Occured During Update The Department ");
+
+                return View(department);
+
+            }
         }
     }
 }
